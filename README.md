3fe7050623f317617fb98e461789d5fef836574a
Crisp-LIN <1415239409@qq.com>
2022-10-06 16:00:16 +0800

    ./README.md 增加一行内容测试git命令 ./scripts/argp.py 1.参数echo改为默认参数 2.加入可选参数"b" 3.把两行注释合并为一行 4. 加入变量"args_merge" 5.修改一个内容输出代码

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Zea Mays SNP Calling Project
 20221005
 Project started 2013-01-03
+TODO: ask sequencing center about adapters
--- scripts/argp.py
+++ scripts/argp.py
@@ -2,15 +2,17 @@ import argparse     # 引入模块
 # 建立解析对象
 parser = argparse.ArgumentParser()
  
-parser.add_argument("echo",help = "echo is here~",type = int)   # a)xx.add_argument("aa") 给xx对象增加一个aa属性;当前情况下为给parser对象增加一个"echo"属性 b)type = int 规定可传入参数的类型
+parser.add_argument("--echo",help = "echo is here~",type = int)   # a)xx.add_argument("aa") 给xx对象增加一个aa属性;当前情况下为给parser对象增加一个"echo"属性 b)type = int 规定可传入参数的类型
 parser.add_argument('--algo', help='algorithm',
                     choices=['DQN', 'Double', 'Dueling','DuelingDouble'],
                     default='DQN')
-
+parser.add_argument('--b', help='输入点什么',
+                    default='默认值')
  
-# 把parser中设置的所有"add_argument"给返回到args子类对象当中
-# 那么parser中增加的属性内容都会在args对象中，使用即可。·
+# 把parser中设置的所有"add_argument"给返回到args子类对象当中,那么parser中增加的属性内容都会在args对象中，使用即可。·
 args = parser.parse_args()
 
 # print(args.echo)     # 打印定位参数echo
-print(args.algo+"测试")
+args_merge = args.algo + args.b
+print("测试：" + args_merge)
+
